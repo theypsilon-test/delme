1c5a7d279abf813b5005cfeafd1653ba15e8fa52
José Manuel Barroso Galindo <theypsilon@gmail.com>
2022-11-19 20:37:58 +0100

    Ok

--- .github/experiment.py
+++ .github/experiment.py
@@ -21,9 +21,7 @@ class Repo:
     branch: str
     result: int = None
     process: Popen = None
-
-
-g = Github(os.environ['GITHUB_TOKEN'], pool_size=100)
+    files = {}
 
 def main():
 
@@ -32,7 +30,7 @@ def main():
     repos = []
     
     repo_count = 0
-    for repo in g.get_user('MiSTer-devel').get_repos():
+    for repo in Github(os.environ['GITHUB_TOKEN']).get_user('MiSTer-devel').get_repos():
         repo_path = 'asdf/dist_repo_%s' % repo.name
         lower_name = repo.name.lower()
         if lower_name in ('distribution_mister', 'downloader_mister') or not lower_name.endswith('mister') or 'linux' in lower_name or 'sd-install' in lower_name:
@@ -49,8 +47,16 @@ def main():
             break
     
     if not repos_downloaded(repos):
-        raise Exception('Some repos didnt download.')
+        raise Exception('Some repos didnt download!')
 
+    for repo in repos:
+        list_repository_files(repo.files, repo_path)
+
+    for repo in repos:
+        print(repo.name)
+        print(repo.files)
+
+    print()
     print("Time:")
     end = time.time()
     print(end - start)
@@ -72,8 +78,6 @@ def process_repos(all_repos: List[Repo]):
                 count += 1
                 repo.result = result
                 repo.process = None
-            if result == 0:
-                print(repo.name)
 
 def repos_downloaded(repos: List[Repo]):
     for repo in repos:
@@ -83,39 +87,19 @@ def repos_downloaded(repos: List[Repo]):
     
     return True
 
-def list_repository_files(repo_path, repo_https_url):
-    shutil.rmtree(repo_path, ignore_errors=True)
-    error = None
-    for retry in range(5):
-        try:
-            repo = Repo.clone_from(repo_https_url, repo_path, depth=1)
-            error = None
-            break
-        except Exception as e:
-            print('Retry! %s' % retry)
-            error = e
-            time.sleep(1)
+def list_repository_files(files, path):
 
-    if error is not None:
-        raise error
-
-    return [(repo_https_url, repo_https_url, repo_https_url)]
-
-    files = []
     contents = []
-    for content_folder in ['releases', 'Palette', 'Palettes', 'palette', 'palettes']:
-        try:
-            contents = [*contents, *[[content, content_folder.lower()] for content in repo.get_contents(content_folder)]]
-        except UnknownObjectException:
-            pass
+    for content_folder in ['releases', 'palette', 'palettes', 'docs']:
+        contents.append(('%s/%s' % (path, content_folder), content_folder))
 
     while contents:
         file_content, content_folder = contents.pop(0)
-        if file_content.type == "dir":
-            contents = [*contents, *[[content, content_folder] for content in repo.get_contents(file_content.path)]]
+        if Path(file_content).is_dir():
+            contents = [*contents, *[[content, content_folder] for content in os.walk(file_content)]]
         else:
-            files.append([content_folder, file_contentgo.path, file_content.download_url])
-    return files
+            files[content_folder] = files.get(content_folder, [])
+            files[content_folder].append(file_content)
 
 if __name__ == '__main__':
     main()
